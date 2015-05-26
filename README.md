25fe9692059a2906cbe5e7f5032502493591fec3
MarcelloPichini <marcello.pichini@gmail.com>
2015-05-26 15:56:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+This program allows you to store in a MySQL database texts and comments of ".doc files" saved within a folder. The program also works with folders nested in several levels.
+
+doc_file_archiver uses the "yomu" gem (version 0.2.2).
+
+
+1)Make sure that gem "yomu" is installed on your system. If not, type this on the shell:
+
+	sudo gem install yomu
+
+
+
+2)Before running the program you need to create a database named "doc_db":
+
+	CREATE DATABASE doc_db;
+	
+	
+
+3)The program uses the following data for MySql access:
+
+	user: script
+	
+	pass: password
+	
+so create a user like this or change the source code where needed to use a different user.
+
+
+
+4)Make sure that the user has permissions to modify the database: 
+
+	GRANT ALL ON doc_db.* to script@localhost;
+
+
+
+Collaborators: Federico Cioschi, Lorenzo Camaione, Marcello Pichini, Vasco Coelho. 
---
+++ archiver.rb
@@ -0,0 +1,45 @@
+
+require 'yomu'
+require 'mysql'
+
+begin
+	#script-->user, password--> password of 'script' user, doc_db--> database name
+	con = Mysql.new 'localhost', 'script', 'password', 'doc_db'
+	
+	
+	check = con.query("SHOW TABLES LIKE \"doc_table\";")
+	if check.num_rows==0
+		con.query("create table doc_table(id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, file_name varchar(255), doc_text text, doc_comment text);")
+		puts "\"doc_table\" created\n"
+	end
+
+	puts "Type the folder path:"
+	path = gets.chomp
+
+	directory = Dir["#{path}/**/*.doc"]
+
+	i=1
+
+	directory.each do |f|
+		yomu = Yomu.new "#{f}"
+		text = yomu.text.gsub('"', '\"')
+		comment = yomu.metadata['Comments'].gsub('"', '\"')
+	
+		input = "INSERT INTO doc_table (file_name, doc_text, doc_comment) VALUES(\"#{f}\", \"#{text}\", \"#{comment}\");"
+		con.query(input)
+		
+		puts "#{i}) #{f} -->done"
+		i+=1
+	
+	end
+
+	rescue Mysql::Error => e
+		puts e.errno
+		puts e.error
+    
+	ensure
+		con.close if con
+end
+
+
+
